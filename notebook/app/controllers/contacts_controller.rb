fce43ac9b90e3cc12404de60a5988ca3f87b3dd0
Willian Yassunaka <willianhy@gmail.com>
2017-08-31 01:41:23 +0000

    basic auth

--- notebook/app/controllers/contacts_controller.rb
+++ notebook/app/controllers/contacts_controller.rb
@@ -1,6 +1,7 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
   before_action :set_options_for_select, only: [:new, :edit, :update, :create]
+  http_basic_authenticate_with name: "willian", password: "123", only: "destroy"
 
   # GET /contacts
   # GET /contacts.json
